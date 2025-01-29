535497ea845aac40218ae6f497cdd64760afea86
rajasekhar <raj479.mits@gmail.com>
2025-01-30 01:22:17 +0530

    session-24

---
+++ count/variables.tf
@@ -0,0 +1,19 @@
+variable "instances" {
+    default = ["mysql", "backend", "frontend"]
+}
+
+variable "zone_id" {
+    default = "Z02394622J1SSYR6C9O0N"
+}
+
+variable "domain_name" {
+    default = "raj82s.online"
+}
+
+variable "common_tags" {
+    type = map
+    default = {
+        Project = "expense"
+        Environment = "dev"
+    }
+}
